6327fdca7ab18e6a504f939ec0e52175e207b17f
Marcin Wdowiak <mardowiak@gmail.com>
2020-09-15 21:24:49 +0200

    playing with JS

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ age=25;
 console.log('Auuuuuuuuuuuuu');
 console.log(`Cześć, jestem ${name} i mam ${age} lat.`);
 console.log('Chuj im w ryj, kurwa!!!!!!!!!!');
+
+const connection= document.querySelector('.empty-class__js')
+console.log(connection);
+connection.innerHTML=`Takie tam, łączenie się JS-em. Auuuuu! `;
